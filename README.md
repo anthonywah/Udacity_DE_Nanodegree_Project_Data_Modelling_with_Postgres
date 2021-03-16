7d73dce91a2eaff456c7639b063e195c33bce48d
WahChiShing <wahchishing@gmail.com>
2021-03-16 09:03:36 +0000

    Re-submission

--- README.md
+++ README.md
@@ -3,35 +3,70 @@
 
 The main goal of this database design is to query:
 
-1) Users data,
-2) Songs data, and
-3) Users song-playing activities
+1. Users data,
+2. Songs data, and
+3. Users song-playing activities
 
 conveniently for analysis. In this project, we have adpoted a star-schema to design the database.
 
+### Requirements
 
-### "songplays" Table
+`Python 3.6.3` is the required python version.
+
+Other required python libraries are detailed in `requirements.txt`
+
+
+### Instructions
+
+`data` directory contains the data needed for the ETL processes. Add more json files to the corresponding directory to load more data into the database. 
+
+To directly load data into the database, run the following command:
+
+> `python etl.py`
+
+
+To load data into the database, and have brief summary on the database, open `test.ipynb` and run the cells one by one.
+
+
+### Database Structure / Schema
+
+##### `songplays` Table
 
 Fact of the star-schema design. It holds all the points that can be referenced by other dimension tables (to be discussed below), such as `song_id`, `user_id`, `artist_id`, ... etc.
 
 
-### "users" Table
+##### `users` Table
 
 One of the dimension tables that holds the details of each user. Referenced by `songplays` table using `user_id`.
 
 
-### "songs" Table
+##### `songs` Table
 
 One of the dimension tables that holds the details of all songs in the database. Referenced by `songplays` table using `song_id`.
 
 
-### "artists" Table
+##### `artists` Table
 
 One of the dimension tables that holds the details of each artist of all songs. Referenced by `songplays` table using `artist_id`.
 
 
-### "time" Table
+##### `time` Table
 
 Last dimension table that holds time data. It is referenced by the `start_time` timestamp in the `songplays` table and used to facilitate time aggregation queries. 
 
+
+### Sample Query
+
+For example, to find out how many times a song is played, one can run this query:
+
+`SELECT songs.title, COUNT(songplays.song_id) as played_times 
+FROM (songplays JOIN songs ON songplays.song_id = songs.song_id) 
+GROUP BY songs.title LIMIT 5;`
+
+The following output will be returned
+
+
+| title          | played_times |
+| -------------- | ------------ |
+| Setanta matins | 1            |
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,38 +1,115 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table songplays"
-user_table_drop = "DROP table users"
-song_table_drop = "DROP table songs"
-artist_table_drop = "DROP table artists"
-time_table_drop = "DROP table time"
+songplay_table_drop = """DROP table songplays"""
+user_table_drop = """DROP table users"""
+song_table_drop = """DROP table songs"""
+artist_table_drop = """DROP table artists"""
+time_table_drop = """DROP table time"""
 
 # CREATE TABLES
 
-songplay_table_create = 'CREATE TABLE IF NOT EXISTS "songplays" (songplay_id int, start_time timestamp, user_id int, level text, song_id text, artist_id text, session_id int, location text, user_agent text);'
+songplay_table_create = """CREATE TABLE IF NOT EXISTS "songplays" (
+    songplay_id SERIAL, 
+    start_time timestamp NOT NULL, 
+    user_id int NOT NULL, 
+    level text, 
+    song_id text, 
+    artist_id text, 
+    session_id int NOT NULL, 
+    location text, 
+    user_agent text, 
+    PRIMARY KEY (songplay_id));"""
 
-user_table_create = 'CREATE TABLE IF NOT EXISTS "users" (user_id int, first_name text, last_name text, gender text, level text);'
+user_table_create = """CREATE TABLE IF NOT EXISTS "users" (
+    user_id int, 
+    first_name text, 
+    last_name text, 
+    gender text, 
+    level text, 
+    PRIMARY KEY (user_id));"""
 
-song_table_create = 'CREATE TABLE IF NOT EXISTS "songs" (song_id text, title text, artist_id text, year int, duration float);'
+song_table_create = """CREATE TABLE IF NOT EXISTS "songs" (
+    song_id text, 
+    title text, 
+    artist_id text, 
+    year int, 
+    duration float, 
+    PRIMARY KEY (song_id));"""
 
-artist_table_create = 'CREATE TABLE IF NOT EXISTS "artists" (artist_id text, name text, location text, latitude int, longitude int);'
+artist_table_create = """CREATE TABLE IF NOT EXISTS "artists" (
+    artist_id text, 
+    name text, 
+    location text, 
+    latitude int, 
+    longitude int, 
+    PRIMARY KEY (artist_id));"""
 
-time_table_create = 'CREATE TABLE IF NOT EXISTS "time" (start_time timestamp, hour int, day int, week int, month int, year int, weekday int);'
+time_table_create = """CREATE TABLE IF NOT EXISTS "time" (
+    start_time timestamp, 
+    hour int, 
+    day int, 
+    week int, 
+    month int, 
+    year int, 
+    weekday int, 
+    PRIMARY KEY (start_time));"""
 
 # INSERT RECORDS
 
-songplay_table_insert = 'INSERT INTO "songplays" (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
+songplay_table_insert = """INSERT INTO "songplays" (
+    start_time, 
+    user_id, 
+    level, 
+    song_id, 
+    artist_id, 
+    session_id, 
+    location, 
+    user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
-user_table_insert = 'INSERT INTO "users" (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s)'
+user_table_insert = """INSERT INTO "users" (
+    user_id, 
+    first_name, 
+    last_name, 
+    gender, 
+    level) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) 
+    DO 
+        UPDATE SET level = EXCLUDED.level;"""
 
-song_table_insert = 'INSERT INTO "songs" (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)'
+song_table_insert = """INSERT INTO "songs" (
+    song_id, 
+    title, 
+    artist_id, 
+    year, 
+    duration) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) 
+    DO NOTHING"""
 
-artist_table_insert = 'INSERT INTO "artists" (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)'
+artist_table_insert = """INSERT INTO "artists" (
+    artist_id, 
+    name, 
+    location, 
+    latitude, 
+    longitude) VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) 
+    DO NOTHING"""
 
-time_table_insert = 'INSERT INTO "time" (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)'
+time_table_insert = """INSERT INTO "time" (
+    start_time, 
+    hour, 
+    day, 
+    week, 
+    month, 
+    year, 
+    weekday) VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) 
+    DO NOTHING"""
 
 # FIND SONGS
 
-song_select = 'SELECT songs.song_id, songs.artist_id FROM (songs JOIN artists ON songs.artist_id = artists.artist_id) WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s'
+song_select = """SELECT songs.song_id, songs.artist_id FROM (
+    songs JOIN artists ON songs.artist_id = artists.artist_id) 
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s"""
 
 # QUERY LISTS
 
